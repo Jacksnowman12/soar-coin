2795d215adf06268f549c0a75b66186b1db2fae2
Jacksnowman12 <benzhou2005@gmail.com>
2018-01-12 14:04:14 -0500

    OOOF
    OOOF

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -2,7 +2,6 @@
 # Copyright (c) 2012-2016 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 if [ $# -gt 1 ]; then
     cd "$2" || exit 1
 fi
